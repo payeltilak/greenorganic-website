96a275b048a5acfbdc251a1c0d19f06f306f3000
unknown <you@example.com>
2022-05-28 16:57:04 +0600

    Update  quantity done

--- src/Pages/UpdateItem/UpdateItem.js
+++ src/Pages/UpdateItem/UpdateItem.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 const UpdateItem = () => {
     const {id}=useParams()
     const [item, setItem] = useState()
+    const quantityRef=useRef('')
     
     useEffect(() => {
         const url = `http://localhost:5000/inventories/${id}`
@@ -30,13 +31,34 @@ const UpdateItem = () => {
             toast.success("Product delivered")
         })
     }
+    const addQuantity = (e) => {
+        e.preventDefault()
+        const newQuantity=parseInt(quantityRef.current.value)
+        const itemQuantity = item.quantity
+        const updateQuantity=newQuantity+itemQuantity
+        const quantity = { ...item, quantity: updateQuantity }
+        const url =`http://localhost:5000/update-inventories/${item._id}`
+        fetch(url, {
+            method: "PUT",
+            headers: {
+                'content-type':"application/json"
+            },
+            body:JSON.stringify(quantity)
+        })
+        .then(res=>res.json())
+            .then(data => {
+            toast.success("Added Quantity")
+            e.target.reset()
+        })
+
+    }
     return (
         <div>
             <img src={item?.img} alt="" />
             <p>Quantity: {item?.quantity}</p>
             <button onClick={()=>handleDelivered(item?._id)} className='btn btn-success'>Delivered</button>
-            <form action="">
-                <input type="text" />
+            <form onSubmit={addQuantity}>
+                <input type="number" ref={quantityRef}/>
                 <input type="submit" value="Update Product" />
             </form>
         </div>
